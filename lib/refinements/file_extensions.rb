c73911c7a2f264ac7936955ef421c8637d500def
Michael Chui <saraid216@gmail.com>
2016-02-05 14:20:39 -0800

    add a filepath guesser

---
+++ lib/refinements/file_extensions.rb
@@ -0,0 +1,15 @@
+class File
+  def self.[](path, mode = 'r')
+    file_handle = nil
+    [
+      lambda { |path| path },
+      lambda { |path| File.expand_path(path) },
+      lambda { |path| File.join(Dir.pwd, path) }
+    ].each do |strategy|
+      filename = strategy.call(path)
+      file_handle = File.open(filename, mode) if File.exists? filename
+    end
+    file_handle
+  end
+end
+
