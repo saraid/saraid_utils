53df39f02e56c6b5678191a310f8d4378117c3e7
Michael Chui <saraid216@gmail.com>
2016-09-29 12:55:51 -0700

    ARGV.consume

--- lib/refinements/argv_extensions.rb
+++ lib/refinements/argv_extensions.rb
@@ -2,4 +2,28 @@ class << ARGV
   def get(regex, default = nil)
     self.grep(regex).first || default
   end
+
+  # Based off: http://faculty.knox.edu/dbunde/teaching/chapel/#Declaration%20Modifiers
+  def consume(*args)
+    variable_name = args[0]
+    type = args[1]
+    default = args[2]
+
+    value = grep(/--#{variable_name}=(.*)/).first
+    unless value.nil?
+      value = value.sub(/.*=/, '')
+    end
+
+    begin
+      if Kernel.respond_to?(type.name.to_sym)
+        Kernel.send(type.name.to_sym, value)
+      elsif type.respond_to?(:from_s)
+        type.from_s(value)
+      elsif type.respond_to?(:try_convert)
+        type.try_convert(value)
+      end
+    rescue
+      default
+    end || default
+  end
 end
