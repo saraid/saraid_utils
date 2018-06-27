eede7a5c01a1fbbd883cb96fc1332e682850a51d
Michael Chui <saraid216@gmail.com>
2018-06-27 16:27:12 -0700

    Kernel#require_stdlibs; Kernel#require_gems

---
+++ lib/refinements/kernel_extensions.rb
@@ -0,0 +1,40 @@
+module Kernel
+  def require_stdlibs(*libs)
+    libs.each(&Kernel.method(:require))
+  end
+
+  # For when you don't want a Bundler dependency.
+  #
+  # Usage:
+  # require_gems(
+  #   { ::activesupport => 'activesupport/core_ext/numeric/time' },
+  #   'table_print',
+  #   :rest-client
+  # )
+  def require_gems(*gems)
+    missing_gems = gems.
+      map { |gem| Hash === gem ? gem.keys : gem }.
+      flatten.
+      map(&:to_s).
+      reject do |gem|
+        Gem::Specification.all_names.any? { |name| name.start_with?(gem) }
+      end
+
+    unless missing_gems.empty?
+      puts 'Gems missing. Please run:'
+      puts '```'
+      missing_gems.each do |gem|
+        puts "  gem install #{gem}"
+      end
+      puts '```'
+      raise LoadError, missing_gems.map(&:to_s)
+    end
+
+    gems.each do |gem|
+      case gem
+      when String, Symbol then require gem.to_s
+      when Hash then gem.values.each(&Kernel.method(:require))
+      end
+    end
+  end
+end
