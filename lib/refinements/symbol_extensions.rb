d86ae8deb7151e12f56ff4b6d14377874ec88942
Michael Chui <saraid216@gmail.com>
2018-07-17 15:27:57 -0700

    Symbol#to_method

--- lib/refinements/symbol_extensions.rb
+++ lib/refinements/symbol_extensions.rb
@@ -2,4 +2,12 @@ class Symbol
   def call(*args, &block)
     ->(caller, *rest) { caller.send(self, *rest, *args, &block) }
   end
+
+  # case SomeObject
+  # when :foo.to_method then :foo
+  # when :bar.to_method(:baz) then :barbaz
+  # end
+  def to_method(*args, &block)
+    proc { |obj| obj.send(self, *args, &block) }
+  end
 end
