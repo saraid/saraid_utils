55c5a139706267d632d10435f13a1b50a5be6ca2
Michael Chui <saraid216@gmail.com>
2018-11-26 16:24:32 -0800

    not sure why i didnt alias to begin with

--- lib/refinements/symbol_extensions.rb
+++ lib/refinements/symbol_extensions.rb
@@ -2,9 +2,6 @@ class Symbol
   def call(*args, &block)
     ->(caller, *rest) { caller.send(self, *rest, *args, &block) }
   end
-
-  def as_method
-    call
-  end
-  alias to_method as_method
+  alias as_method call
+  alias to_method call
 end
