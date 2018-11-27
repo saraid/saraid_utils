1ddd381dc30eed1e88b53fea9984fbd072648ea1
Michael Chui <saraid216@gmail.com>
2018-11-26 16:17:17 -0800

    move boolean_attr out into a module

---
+++ lib/refinements/boolean_attribute_accessors.rb
@@ -0,0 +1,70 @@
+module BooleanAttributeAccessors
+  def boolean_attr_reader(*names)
+    names.each do |name|
+      case name
+      when Hash
+        name.each do |positive, negative|
+          boolean_attribute_reader(positive)
+          boolean_attribute_reader(negative, positive)
+        end
+      else
+        boolean_attribute_reader(name)
+      end
+    end
+  end
+
+  def boolean_attr_writer(*names)
+    names.each do |name|
+      case name
+      when Hash
+        name.each do |positive, negative|
+          boolean_attribute_writer(positive)
+          boolean_attribute_writer(negative, positive, false)
+        end
+      else
+        boolean_attribute_writer(name)
+      end
+    end
+  end
+
+  def boolean_attr_accessor(*names)
+    boolean_attr_reader(*names)
+    boolean_attr_writer(*names)
+  end
+
+  def boolean_attr(name, negate_as: nil)
+    boolean_attribute_reader(name)
+    negated_boolean_attribute_reader(negate_as, name) unless negate_as.nil?
+    boolean_attribute_writer(name)
+    boolean_attribute_writer(negate_as, name, false) unless negate_as.nil?
+  end
+
+  private def boolean_attribute_reader(name, varname = name)
+    return if method_defined?(:"#{name}?")
+    define_method(:"#{name}?") { instance_variable_get(:"@#{varname}") == true }
+  end
+
+  private def negated_boolean_attribute_reader(name, being_negated)
+    return if method_defined?(:"#{name}?")
+    define_method(:"#{name}?") { !send(:"#{being_negated}?") }
+  end
+
+  private def boolean_attribute_writer(name, varname = name, to_value = true)
+    return if method_defined?(:"#{name}!")
+    define_method(:"#{name}!") { instance_variable_set(:"@#{varname}", to_value) }
+  end
+end
+
+module DemonstratingSaraidUtils
+  class TestFoo
+    extend BooleanAttributeAccessors
+
+    boolean_attr_accessor :valid => :invalid
+    boolean_attr_accessor :tbd, :initialized
+    boolean_attr_accessor :done, :ready => :pending, :awesome => :terrible
+    boolean_attr :fun, negate_as: :boring
+
+    [:valid?, :invalid?, :tbd?, :initialized?, :done?, :ready?, :pending?, :awesome?, :terrible?, :fun?, :boring?].
+      each { |m| instance_methods.include?(m) || (raise "#{m} is not defined and should be.") }
+  end
+end
--- lib/refinements/class_extensions.rb
+++ lib/refinements/class_extensions.rb
@@ -9,48 +9,4 @@ class Class
       end
     end
   end
-
-  def boolean_attr_reader(*names)
-    names.each do |name|
-      case name
-      when Hash
-        name.each do |positive, negative|
-          define_method(:"#{positive}?") { instance_variable_get(:"@#{positive}") == true }
-          define_method(:"#{negative}?") { !send(:"#{positive}?") }
-        end
-      else
-        define_method(:"#{name}?") { instance_variable_get(:"@#{name}") == true }
-      end
-    end
-  end
-
-  def boolean_attr_writer(*names)
-    names.each do |name|
-      case name
-      when Hash
-        name.each do |positive, negative|
-          define_method(:"#{positive}!") { instance_variable_set(:"@#{positive}", true) }
-          define_method(:"#{negative}!") { instance_variable_set(:"@#{positive}", false) }
-        end
-      else
-        define_method(:"#{name}!") { instance_variable_set(:"@#{name}", true) }
-      end
-    end
-  end
-
-  def boolean_attr_accessor(*names)
-    boolean_attr_reader *names
-    boolean_attr_writer *names
-  end
-end
-
-module DemonstratingSaraidUtils
-  class TestFoo
-    boolean_attr_accessor :valid => :invalid
-    boolean_attr_accessor :tbd, :initialized
-    boolean_attr_accessor :done, :ready => :pending, :awesome => :terrible
-
-    [:valid?, :invalid?, :tbd?, :initialized?, :done?, :ready?, :pending?, :awesome?, :terrible?].
-      each { |m| instance_methods.include?(m) || (raise "#{m} is not defined and should be.") }
-  end
 end
