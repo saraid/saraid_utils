944b8027fd4081962232f2f3b157f2e9a28ddc40
Michael Chui <saraid216@gmail.com>
2019-02-14 12:12:36 -0800

    more into ForwardCompatibility

--- lib/refinements/forward_compatibility.rb
+++ lib/refinements/forward_compatibility.rb
@@ -36,15 +36,55 @@ module Refinements
         end
       end
 
+      unless ForwardCompatibility.instance_method_defined?(Hash, :transform_keys)
+        refine Hash do
+          def transform_keys
+            map { |(key, value)| [ yield(key), value ] }.to_h
+          end
+
+          def transform_values
+            map { |(key, value)| [ key, yield(value) ] }.to_h
+          end
+        end
+      end
+
       # String#delete_prefix #delete_suffix
       # Array#prepend/append
-      # Hash#transform_keys/values
       # Dir.children/each_child
     end
 
     module Ruby26
-      # Proc composition
-      # Enumerable#chain
+      unless ForwardCompatibility.instance_method_defined?(Object, :then)
+        refine Object do
+          alias then yield_self
+        end
+      end
+
+      unless ForwardCompatibility.instance_method_defined?(Proc, :>>)
+        refine Proc do
+          def >>(other_proc)
+            proc { |*args, &block| other_proc.call(self.call(*args, &block)) }
+          end
+
+          def <<(other_proc)
+            proc { |*args, &block| self.call(other_proc.call(*args, &block)) }
+          end
+        end
+      end
+
+      unless ForwardCompatibility.instance_method_defined?(Enumerator, :chain)
+        refine Enumerator do
+          def chain(other)
+            Enumerator.new { |y|
+              self.each { |v| y.yield(v) }
+              other.each { |v| y.yield(v) }
+            }
+          end
+
+          alias :+ :chain
+        end
+      end
+
       # Hash#merge
       # Array#union, Array#difference
       # Range#%
@@ -54,11 +94,13 @@ module Refinements
       unless ForwardCompatibility.instance_method_defined?(Enumerable, :tally)
         refine Array do
           def tally
+            group_by(&:itself).transform_values(&:size)
           end
         end
 
         refine Hash do
           def tally
+            to_a.tally
           end
         end
       end
