0c65703ea23b1d1065cbe284950b332c68fcf450
Michael Chui <saraid216@gmail.com>
2020-03-14 20:40:07 -0700

    time -> plusminus

--- lib/refinements/time_extensions.rb
+++ lib/refinements/time_extensions.rb
@@ -1,8 +1,14 @@
-class Time
+module PlusMinus
   def plusminus(interval)
     [-1, 1]
       .map { |polarity| interval * polarity }
       .map { |delta| self + delta }
       .tap { |ary| ary.singleton_class.define_method(:to_range) { first...last } }
   end
+  alias ± plusminus
+
+  module Refinements refine Time do include PlusMinus end end
 end
+
+#class Time; include PlusMinus; end
+#class Date; include PlusMinus; end
